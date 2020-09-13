763f97a489b7c2a93bda6ef375ba536d2a723688
summerUniq <1152972727@qq.com>
2020-09-13 17:39:27 +0800

    formItem, KInput组件的实现

--- src/components/AndFormTest.js
+++ src/components/AndFormTest.js
@@ -1,45 +1,77 @@
 import React, { Component } from 'react'
-import { Button } from 'antd'
+import { Button} from 'antd'
+import {GithubFilled, LockTwoTone} from '@ant-design/icons'
+import "antd/dist/antd.css"
+
+// 包裹子组件，展示错误信息
+class FormItem extends Component {
+    render() {
+        return (
+            <div className="formItem">
+                {this.props.children}
+                {this.props.validateStatus === 'error' && (
+                    <p style={{ color: 'red' }}>{this.props.message}</p>
+                )}
+            </div>
+        );
+    }
+}
+
+// KInput 组件
+class KInput extends Component {
+
+    render() {
+        const { name, onChange, value, prefix, placeholder, type, onFocus } = this.props
+        return (<div>
+            {prefix}
+            <input name={name} type={type} value={value} placeholder={placeholder} onChange={onChange} onFocus={onFocus} />
+        </div>);
+    }
+}
+
 
 class KForm extends Component {
     handleSubmit = () => {
-        const {validateFields} = this.props
+        const { validateFields } = this.props
         validateFields && validateFields((isValid, data) => {
             if (isValid) {
                 console.log('提交数据', data);
-            }else{
+            } else {
                 console.log('提交失败', data);
                 alert('校验失败')
             }
         })
     }
     render() {
-        const {getFieldDecorator} = this.props
+        const { getFieldDecorator, getFieldError, isFieldTouched } = this.props
+        const userNameError = isFieldTouched('userName') && getFieldError('userName')
+        const pwdError = isFieldTouched('password') && getFieldError('password')
         return (
             <div>
-                <div>
-                {getFieldDecorator("userName", {
+                <FormItem validateStatus={userNameError ? 'error' : ''} message={userNameError || ''}>
+                    {getFieldDecorator("userName", {
                         rules: [{
                             required: true, message: "Please input your username!"
                         }]
                     })(
-                        <input type="text" />
+                    <KInput type="text" prefix={<GithubFilled /> } />
+                        
                     )}
-                    
-                </div>
 
-                <div>
-                {getFieldDecorator("password", {
+                </FormItem>
+
+                <FormItem validateStatus={pwdError ? 'error' : ''} message={pwdError || ''}>
+                    {getFieldDecorator("password", {
                         rules: [{
                             required: true, message: "Please input your password!"
                         }]
                     })(
-                        <input type="password" />
+                        <KInput type="password" prefix={<LockTwoTone /> } />
                     )}
-                    
-                </div>
 
-                <Button onClick = {this.handleSubmit}>登陆</Button>
+                </FormItem>
+
+                <Button onClick={this.handleSubmit}>登陆</Button>
             </div>
         )
     }
@@ -56,7 +88,7 @@ function kFormCreate(Comp) {
         }
         handleOnChange = (e) => {
 
-            const {name, value} = e.target
+            const { name, value } = e.target
             // 获取input输入框的值存储到state中
             this.setState({
                 [name]: value
@@ -66,18 +98,29 @@ function kFormCreate(Comp) {
             })
         }
 
+        handleOnFocus = (e) => {
+            const { name } = e.target
+            this.setState({
+                [name + 'Focus']: true
+            })
+        }
+
+        isFieldTouched = (field) => {
+            return !!this.state[field + 'Focus']
+        }
+
 
         validateField = (field) => {
             const rules = this.options[field].rules // 获取校验规则
             console.log('rules: ', rules);
-            
+
             const ret = !rules.some((rule) => {
                 if (rule.required) {
                     // 校验必填项
                     if (!this.state[field]) {
                         this.setState({
                             // 错误信息设置
-                            [field + 'Message'] : rule.message
+                            [field + 'Message']: rule.message
                         })
                         return true
                     }
@@ -87,7 +130,7 @@ function kFormCreate(Comp) {
             // 若校验成功，清除错误信息
             if (ret) {
                 this.setState({
-                    [field + "Message"] : ''
+                    [field + "Message"]: ''
                 })
             }
 
@@ -102,7 +145,8 @@ function kFormCreate(Comp) {
                     React.cloneElement(inputComp, {
                         name: field,
                         value: this.state[field] || '', // 控件初始值
-                        onChange: this.handleOnChange
+                        onChange: this.handleOnChange,
+                        onFocus: this.handleOnFocus
                     })
                 }
             </div>)
@@ -118,12 +162,18 @@ function kFormCreate(Comp) {
             cb(ret, this.state)
         }
 
+        getFieldError = (field) => {
+            return this.state[field + 'Message']
+        }
+
         render() {
             return (
                 <Comp
                     {...this.props}
-                    getFieldDecorator = {this.getFieldDecorator}
-                    validateFields = {this.validateFields}
+                    getFieldDecorator={this.getFieldDecorator}
+                    validateFields={this.validateFields}
+                    getFieldError={this.getFieldError}
+                    isFieldTouched={this.isFieldTouched}
                 />
             )
         }
